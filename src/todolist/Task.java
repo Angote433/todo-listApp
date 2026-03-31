1fb16a5ded270f5994c8776c73eadf323f37a2a5
Arnold Angote <arnoldangote9@gmail.com>
2026-03-31 09:33:41 +0300

    Created a new class for the structure of each task

---
+++ src/todolist/Task.java
@@ -0,0 +1,32 @@
+package todolist;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    /*
+    structure of each task.,for creating eeach task object.
+     */
+    private String name;
+    private String description;
+    private LocalDateTime date;
+    boolean isComplete;
+
+    public Task(String name,String description,LocalDateTime date){
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.isComplete = false;
+    }
+
+    public void setName(String name){this.name = name;}
+    public String getName(){return name;}
+
+    public void setDescription(String description){this.description = description;}
+    public String  getDescription(){return description;}
+
+    public void setDate(LocalDateTime date){this.date = date;}
+    public LocalDateTime getDate(){return date;}
+
+    public void setComplete(boolean isComplete){this.isComplete = isComplete;}
+    public boolean isComplete(){return isComplete;}
+}
